030788eec73e2cc87181e91aec8ea9abba2d3c2d
Lucas Vieira <vieira.lucasmm@gmail.com>
2018-11-05 21:46:43 -0200

    add global variables

--- main.cpp
+++ main.cpp
@@ -34,3 +34,10 @@ void cabecalho();
 float distancia_entre_pontos(ponto p1, ponto p2);
 bool intersecao(vetor v1, vetor v2);
 float produto_vetorial(ponto p1, ponto p2);
+
+/*Variáveis globais*/
+int cont = 0; /*Cont é a quantidade de pontos existentes*/
+int tam = 100; /*Tamanho do array que vai guardar os pontos, inicialmente começa com 100 e cresce conforme a necessidade.*/
+int etapa = 1; /*Para controlar a etapa 1 (Construir o pílogono) e a etapa 2 (Escolher um ponto)*/
+ponto p; /*Ponto que será utilizado para descobrir a localização*/
+ponto *pontos = (ponto *) malloc (sizeof(ponto)*tam);/*Alocação de memória para o array de pontos*/
